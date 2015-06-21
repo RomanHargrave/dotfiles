09a45feaa0c9aa2c15e7d9ee474b013532f93788
Roman Hargrave <roman@hargrave.info>
2015-06-21 11:21:51 -0500

    Add support for output files

--- config/fish/lib/logging.fish
+++ config/fish/lib/logging.fish
@@ -1,24 +1,29 @@
+set -g __log_level 1
+
+function __logger_format
+    echo -- (date +'[%H:%M:%S %Y-%m-%d]') $argv
+end
+
 function log -a color -a message
-	set timestamp (date +'[%H:%M:%S %Y-%m-%d]')
+    isatty 2; and set_color $color 1>&2
 	begin
-		isatty 2; and set_color $color
-		echo "-- $timestamp $message"
-		isatty 2; and set_color normal
-	end 1>&2
+		__logger_format $message 
+	end | tee -a $__log_file 1>&2
+	isatty 2; and set_color normal 1>&2
 end
 
 function info
-	log green "$argv"
+    log green "$argv"
 end
 
 function warn 
-	log yellow "$argv"
+    log yellow "$argv"
 end
 
 function error
-	log red "$argv"
+    log red "$argv"
 end
 
 function debug
-	log cyan "$argv"
+    log cyan "$argv"
 end
